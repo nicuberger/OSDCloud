f20044ff8dd7651e61863d1befd9d4a2764b1b65
Nicu Berger <61410367+nicuberger@users.noreply.github.com>
2023-04-12 08:46:38 +0200

    Update Startup.ps1

--- BBZ/Startup.ps1
+++ BBZ/Startup.ps1
@@ -5,6 +5,14 @@ Set-ExecutionPolicy RemoteSigned -Force
 Install-Module AutopilotOOBE -Force
 Import-Module AutopilotOOBE -Force
 
+
+$Params = @{
+    Title = 'OSDeploy Autopilot Registration'
+    AssignedComputerName = 'BBZ-'
+    Hidden = 'AddToGroup','AssignedUser','GroupTag'
+}
+AutopilotOOBE @Params
+
 Start-AutopilotOOBE
 
 Start-OSDCloudGUI
